5abdac0e648e4298d3d14ccb1aaac65964a031c6
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2023-06-11 14:18:54 -0300

    test

---
+++ learning-python-2896241-main/Ch2 - Basics/helloworld_start.py
@@ -0,0 +1,14 @@
+#
+# Example file for HelloWorld
+# LinkedIn Learning Python course by Joe Marini
+#
+
+def main1():
+     name = print("i'm not the test")
+print(name)
+
+# calling the funxction
+main1()
+print(main())
+
+
