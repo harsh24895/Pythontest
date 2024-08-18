5e7c5d8db51e66aa6807d1bdbc67b01629a63a2e
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2024-08-18 14:12:44 -0300

    python autmation learning

---
+++ functions.py
@@ -0,0 +1,7 @@
+def addition():
+    a = 10
+    b = 8
+    print(a + b)
+
+addition()
+
--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def printf(nam):
+    print("nert the name"+nam)
+#*********************************************************************
+name1 = "test"
+working = "add to the log"
+
+#python formating string
+print("{} works and {}".format(name1,working))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    printf('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
